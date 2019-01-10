6d4d7c05ff811b5d84b2060ccdbbe7dcf6aa4c6f
Ronaldo Albertini <ronaldo.albertini@sharecare.com>
2019-01-10 15:26:59 -0200

    Initial Commit

---
+++ findiOSMissingTranslations/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  findiOSMissingTranslations
+//
+//  Created by Ronaldo Albertini on 10/01/19.
+//  Copyright © 2019 Ronaldo Albertini. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
